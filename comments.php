3ca8b2a0add0b2822f987da0724a57e66b15ef50
Lorenzo <60339889+TCBW-KOD@users.noreply.github.com>
2024-04-19 15:12:32 +0800

    Update comments.php
    2024.04.19: Just show nothing when the commment section is closed

--- comments.php
+++ comments.php
@@ -1,6 +1,6 @@
 <div id="comments" class="comments content">
     <?php if (post_password_required()) : ?>
-    <p><?php _e( 'Post is password protected. Enter the password to view any comments.' ,'default'); ?></p>
+    <p><?php _e( 'Post is password protected. Enter the password to view any comments.', 'default' ); ?></p>
     <?php else: ?>
         <?php if (have_comments()) : ?>
             <h2><?php comments_number(__('no responses','default'), __('one response','default'), __('% responses','default')); ?></h2>
@@ -19,12 +19,7 @@
             <?php endif; ?>
         <?php endif; ?>
         <?php if ( !comments_open() && post_type_supports( get_post_type(), 'comments' ) ) : ?>
-			<p>
-			<?php 
-				$comments_closed = sprintf( __( 'Comments Off<span class="screen-reader-text"> on %s</span>','default' ), get_the_title());
-				echo $comments_closed;
-			?>
-			</p>
+            <!-- Comments are closed but no message will be displayed. -->
         <?php else: ?>
             <?php my_comment_form(); ?>
         <?php endif; ?>
